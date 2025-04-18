a06fbceede2d83d027f3b323022ca7aa55c844e0
AH07pro1 <adam.hilali@gmail.com>
2025-04-18 16:14:18 -0400

    database setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SnippetCard from '../components/SnippetCard'
+
+function DashboardPage() {
+  return (
+    <div>
+      <SnippetCard/>
+    </div>
+  )
+}
+
+export default DashboardPage
